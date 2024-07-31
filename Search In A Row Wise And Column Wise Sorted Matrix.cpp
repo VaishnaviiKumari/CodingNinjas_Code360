3ca5025cb96f47e96173f0c762bda3d4a55d2c2c
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2024-07-31 18:56:22 +0530

    Search In A Row Wise And Column Wise Sorted Matrix.cpp

---
+++ Search In A Row Wise And Column Wise Sorted Matrix.cpp
@@ -0,0 +1,30 @@
+//Search In A Row Wise And Column Wise Sorted Matrix
+
+#include <bits/stdc++.h> 
+#include<vector>
+
+pair<int, int> search(vector<vector<int>> matrix, int x)
+{
+    // Write your code here
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int rowIndex = 0;
+    int colIndex = col-1;
+
+    while((rowIndex < row ) && (colIndex >= 0)){
+        int element = matrix[rowIndex][colIndex];
+
+        if(element == x){
+            return {rowIndex, colIndex};
+        }
+        else if(element > x){
+            colIndex--;
+        }
+        else{
+            rowIndex++;
+        }
+    }
+    return {-1, -1};
+}
+
