afaa96b84263c6947c9a9562e488acacc0e4e47f
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2025-08-04 12:13:26 +0530

    54. Spiral Matrix.cpp

--- 54. Spiral Matrix.cpp
+++ 54. Spiral Matrix.cpp
@@ -3,9 +3,7 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-
         vector<int> ans;
-
         int row = matrix.size();
         int col = matrix[0].size();
 
