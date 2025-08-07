e3667a555be33d7c3f991b3eae765b1a8d095f65
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2025-08-07 22:39:32 +0530

    Duplicate In Array.cpp

--- Duplicate In Array.cpp
+++ Duplicate In Array.cpp
@@ -1,16 +1,12 @@
 // Duplicate In Array
-
 int findDuplicate(vector<int> &arr) 
 {
     int ans=0;
     for(int i=0;i<arr.size();i++){
         ans=ans^arr[i];
     }
-
     for(int i=0;i<arr.size();i++){
         ans=ans^i;
     }
-
     return ans;
-	
 }
